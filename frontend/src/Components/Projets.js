5956706c76428e67c5923fddb4e61fe6f2d0562a
Sophie Bartoli <sophie.bartoli2@gmail.com>
2024-05-15 20:32:17 +0200

    Travail sur Projets

--- frontend/src/Components/Projets.js
+++ frontend/src/Components/Projets.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import ProjetsCards from './ProjetsCards';
+import Booki from '../Assets/Images/Booki.webp';
+import Bluel from '../Assets/Images/Bluel.webp';
+import Menu from '../Assets/Images/Menu.webp';
+import Kasa from '../Assets/Images/Kasa.webp';
 
 
 function Projets() {
   return (
-    <div id="projets" className="projetsDiv">
-      <div>
-        <h2> Mes projets... </h2>
-        <ProjetsCards/>
-        <ProjetsCards/>
-        <ProjetsCards/>
-        <ProjetsCards/>
-        <ProjetsCards/>
+    <div id="projets">
+      <h2> Mes projets... </h2>
+      <div className="projetsDiv">
+        <ProjetsCards imgSource={Booki} imgAlt="" imgTitle="Booki" imgText="Site internet de location immobilière."/>
+        <ProjetsCards imgSource={Bluel} imgAlt="" imgTitle="Sophie Bluel" imgText="Site internet de photographie."/>
+        <ProjetsCards imgSource={Menu} imgAlt="" imgTitle="Menu Maker" imgText="Site internet de restauration."/>
+        <ProjetsCards imgSource={Kasa} imgAlt="" imgTitle="Grimoire" imgText="Site internet de notation littéraire."/>
       </div>
     </div>
 
--- frontend/src/Components/ProjetsCards.js
+++ frontend/src/Components/ProjetsCards.js
@@ -1,35 +1,14 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
 
-function Example() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
+function ProjetsCards({imgSource, imgAlt, imgTitle, imgText}) {
   return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+      <div className="card">
+        <button> <img src={imgSource} alt={imgAlt}/> </button>
+        <div className="cardText">
+          <h3>{imgTitle}</h3>
+          <p>{imgText}</p>
+        </div>
+      </div>
   );
 }
 
-export default Example;
+export default ProjetsCards;
