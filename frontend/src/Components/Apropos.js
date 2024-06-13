4928505ea5df0f5de147cf7092fa235cd97f786f
Sophie Bartoli <sophie.bartoli2@gmail.com>
2024-06-14 01:39:06 +0200

    Media queries smartphone et tablette fini

--- frontend/src/Components/Apropos.js
+++ frontend/src/Components/Apropos.js
@@ -3,7 +3,6 @@ import logo from '../Assets/Logos/Logo.webp';
 import coding from '../Assets/Images/Coding.webp';
 import travel from '../Assets/Images/Travel.webp';
 import videogames from '../Assets/Images/Videogames.webp';
-import lanterne1 from '../Assets/Images/Lanterne1.webp';
 import { useState } from 'react';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
@@ -20,8 +19,6 @@ function Apropos () {
   return (
     <div id="apropos" className="aProposDiv">
       <button onClick={Toggle}> <FontAwesomeIcon icon={faAngleLeft} size="4x"/> </button>
-      <img className="lanterneLogo" id="lanterneUne" src={lanterne1} alt=""/>
-      <img className="lanterneLogo" id="lanterneDeux" src={lanterne1} alt=""/>
        {slide ? (
           <div className="firstSlide">
             <div>
@@ -57,8 +54,6 @@ function Apropos () {
           </div>
                 )}
       <button onClick={Toggle}> <FontAwesomeIcon icon={faAngleRight} size="4x"/> </button>
-      <img className="lanterneLogo" id="lanterneTrois" src={lanterne1} alt=""/>
-      <img className="lanterneLogo" id="lanterneQuatre" src={lanterne1} alt=""/>
     </div>
   )
 }
